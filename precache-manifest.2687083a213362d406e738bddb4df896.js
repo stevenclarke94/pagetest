89e0bd0ed42c390d0d7d2d0335ab46e991ffb9ef
stevenclarke94 <stevenjsclarke@yahoo.co.uk>
2020-10-18 14:28:56 +0100

    Updates

---
+++ precache-manifest.2687083a213362d406e738bddb4df896.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e3ffb5162abacf3efa9ade96d64b65",
+    "url": "/pagetest/index.html"
+  },
+  {
+    "revision": "0ba17b835086f4bbf801",
+    "url": "/pagetest/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "99bd2acfefe96f227378",
+    "url": "/pagetest/static/css/main.e19ca974.chunk.css"
+  },
+  {
+    "revision": "0ba17b835086f4bbf801",
+    "url": "/pagetest/static/js/2.536ce95a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pagetest/static/js/2.536ce95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bd2acfefe96f227378",
+    "url": "/pagetest/static/js/main.98f3c7dd.chunk.js"
+  },
+  {
+    "revision": "252bc69e73adae236f86",
+    "url": "/pagetest/static/js/runtime-main.019ea979.js"
+  }
+]);
